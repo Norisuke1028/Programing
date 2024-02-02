15c2284139b33d54d0f4c7526786d6ac2c6fd0cf
Norisuke1028 <138739405+Norisuke1028@users.noreply.github.com>
2024-02-02 14:56:45 +0900

    ヘッダーファイル終了
    ヘッダーファイル終了
    ソースファイルとエラーチェックのみ

--- Game/DriveAndAvoid/Object/Player.cpp
+++ Game/DriveAndAvoid/Object/Player.cpp
@@ -1,1 +1,23 @@
 #include "Player.h"
+#include"../Utility/InputControl.h"
+#include"Dxlib.h"
+
+Player::Player() : is_active(false), image(NULL), location(0.0f), box_size(0.0f),
+angle(0.0f), speed(0.0f), fuel(0.0f), barrier_count(0),
+barrier(nullptr)
+{
+
+}
+
+Player::~Player()
+{
+
+}
+
+//‰Šú‰»ˆ—
+void Player::Initialize()
+{
+	is_active = true;
+	location = Vector2D(320.0f, 380.0f);
+
+}
